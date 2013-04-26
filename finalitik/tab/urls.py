1f7679de521c9b98ebf2ab9acde72db5718de750
Romana Grilj <romana.grilj@gmail.com>
2013-04-26 07:54:54 +0200

    Fixed directory structure.

--- finalitik/tab/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from tab import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
-                       url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
-)
--- tab/urls.py
+++ tab/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.conf.urls import patterns, url
 
 from tab import views
@@ -7,16 +6,3 @@ urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 )
-=======
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-admin.autodiscover()
-
-import views
-
-urlpatterns = patterns('',
-    url(r'^charts', views.grafi),
-    url(r'^users', views.uporabniki),
-    url(r'^', views.index),
-)
->>>>>>> e7629dc5cac9a546e20c5cb4714e1db24fff720a
